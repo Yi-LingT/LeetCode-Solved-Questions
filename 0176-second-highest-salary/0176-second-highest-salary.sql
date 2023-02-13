885eda71fd97f02478f6d5d7d88d3195337f3106
Yi-LingT <yilingtsaipro@gmail.com>
2023-02-13 10:46:46 -0600

    Time: 520 ms (14.38%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,13 +1,10 @@
-
-
 with cte as
 (select salary ,dense_rank()over(order by salary desc) rk
 from Employee
 )
+select 
+    ifnull(
+        (select Max(salary) 
+            from cte
+            where rk=2),null) as SecondHighestSalary 
 
-select Max(salary) as SecondHighestSalary 
-from cte
-where rk=2
-
-/*Use MAX(salary) to aggregate the salary in outer loop.*/
-/*Null situation, get one value either a subquery should be written or an aggregate function (MAX()) on the results should be put*/
