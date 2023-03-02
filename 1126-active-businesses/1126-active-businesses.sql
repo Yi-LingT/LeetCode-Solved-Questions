f5f1636fe5068139ce317f2dc3d483bd3b131dd4
Yi-LingT <yilingtsaipro@gmail.com>
2023-03-02 11:37:27 -0600

    Time: 1437 ms (41.75%), Space: 0B (100.00%) - LeetHub

---
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -0,0 +1,12 @@
+with cte1 as
+(select *,avg(occurences)over(partition by event_type) avg_act
+from  Events )
+
+select business_id 
+from cte1
+where occurences >avg_act
+group by business_id
+having count( event_type)>1
+
+
+#must use group by because having count is aggregation
