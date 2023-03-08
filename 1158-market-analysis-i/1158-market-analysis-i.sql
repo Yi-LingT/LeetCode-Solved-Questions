c62c7d9a3044402bac72deed68ff1cd8bf2766ca
Yi-LingT <yilingtsaipro@gmail.com>
2023-03-08 11:30:34 -0600

    Time: 2362 ms (46.13%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,8 @@
+select user_id as buyer_id,join_date,sum(if(order_id is not null,1,0)) as orders_in_2019 
+from Users U
+left join Orders O
+on U.user_id =O.buyer_id 
+And YEAR(order_date) = '2019'
+group by user_id
+
+
