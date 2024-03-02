cc6f6962cf824ab7f44e557cfb285b1eaab04847
Yi-LingT <yilingtsaipro@gmail.com>
2024-03-02 11:42:09 -0500

    Time: 3199 ms (15.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,6 @@
+select customer_id , count(V.visit_id )-count(transaction_id ) as  count_no_trans
+from Visits V
+left join Transactions T
+on V.visit_id =T.visit_id 
+group by customer_id 
+having count(V.visit_id )<>count(transaction_id )
