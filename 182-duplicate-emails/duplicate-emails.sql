b4e610793682e404405e70cc5299df2346ae8c67
Yi-LingT <yilingtsaipro@gmail.com>
2024-03-05 10:55:46 -0500

    Time: 708 ms (54.06%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,8 +1,3 @@
-with cte as (
-select *,count(email)>1 as count1
-from Person 
-group by email )
-
-select email
-from cte 
-where count1=1
+SELECT email from Person
+group by email
+having count(email) > 1;
