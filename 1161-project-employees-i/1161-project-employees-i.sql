503ac19e62e5e320ff2cdb02c047ff8b0cda6291
Yi-Ling Tsai <yilingtsaipro@gmail.com>
2024-08-25 22:03:49 -0400

    Time: 468 ms (79.68%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select P.project_id , round(avg(experience_years),2) as average_years 
+from Project P
+join Employee E
+on P.employee_id =E.employee_id 
+group by P.project_id  
