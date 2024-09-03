a0b296b8691b10c2b6b972a8d96182ece53bed26
Yi-Ling Tsai <yilingtsaipro@gmail.com>
2024-09-02 22:20:08 -0400

    Time: 3437 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -4,6 +4,6 @@ select P.product_name,sum(O.unit) as unit
 from Products P
 join Orders O
 on P.product_id =O.product_id  
-where O.order_date>='2020-02-01' and  O.order_date<= '2020-02-29'
+where DATEDIFF(O.order_date,'2020-02-01') bETWEEN 0 AND 28
 group by P.product_name
 having  sum(O.unit)>=100
